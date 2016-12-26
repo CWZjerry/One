6b6781c8d157a93778c24f72b37ac7c8e83549c7
曹小泽的mac <caowenze@caowenzejiaowolaixunshan.local>
2016-12-26 13:14:57 +0800

    no message

---
+++ One/One/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  One
+//
+//  Created by Jerry on 2016/12/26.
+//  Copyright © 2016年 曹小泽的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ One/One/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  One
+//
+//  Created by Jerry on 2016/12/26.
+//  Copyright © 2016年 曹小泽的Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
